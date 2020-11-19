d5a800aa151994e42a1c5ce9de3e74be6c264f71
midbel <brun.nicolas21@gmail.com>
2020-11-19 10:41:43 +0100

    distrib log entries via http service

---
+++ cmd/www/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+
+	"github.com/midbel/log"
+	"github.com/midbel/toml"
+)
+
+type Log struct {
+	File    string
+	URL     string
+	Pattern string `toml:"format"`
+	Line    int64
+}
+
+func (g Log) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var (
+		query = r.URL.Query()
+		limit = retrLimit(query)
+	)
+	es, err := g.readEntries(limit, query.Get("filter"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if len(es) == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	json.NewEncoder(w).Encode(es)
+}
+
+func (g Log) readEntries(limit int, filter string) ([]log.Entry, error) {
+	r, err := os.Open(g.File)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	rs, err := log.NewReader(r, g.Pattern, "")
+	if err != nil {
+		return nil, err
+	}
+	es, err := rs.ReadAll()
+	if err != nil && !errors.Is(err, io.EOF) {
+		return nil, err
+	}
+	if limit > 0 && limit < len(es) {
+		es = es[len(es)-limit:]
+	}
+	return es, nil
+}
+
+func retrLimit(q url.Values) int {
+	i, _ := strconv.Atoi(q.Get("limit"))
+	return i
+}
+
+func main() {
+	flag.Parse()
+	config := struct {
+		Addr string
+		Logs []Log `toml:"log"`
+	}{}
+	if err := toml.DecodeFile(flag.Arg(0), &config); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	for _, g := range config.Logs {
+		if i, err := os.Stat(g.File); err != nil || i.IsDir() {
+			fmt.Fprintf(os.Stderr, "%s: file does not exist! (%v)\n", g.File, err)
+			os.Exit(1)
+		}
+		http.Handle(g.URL, wrapHandler(g))
+	}
+
+	if err := http.ListenAndServe(config.Addr, nil); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+}
+
+func wrapHandler(next http.Handler) http.Handler {
+  return allowMethod(next)
+}
+
+func allowMethod(next http.Handler) http.Handler {
+  fn := func(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodGet {
+  		w.WriteHeader(http.StatusMethodNotAllowed)
+  		return
+  	}
+    next(w, r)
+  }
+  return http.HandlerFunc(fn)
+}
